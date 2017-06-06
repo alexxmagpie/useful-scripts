6d3876ffb4eacb0827e41a6aba2b3d99748e2ce7
user <alexxmagpie@gmail.com>
2017-06-06 13:59:26 +0300

    setup djangorestframework

---
+++ setup_drf.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import os
+import pip
+import sys
+import subprocess
+
+
+proj_name = raw_input("Enter new project name: ")
+app_name = raw_input("Enter first app name (or leave it empty to ignore creation): ")
+
+
+os.makedirs(proj_name)
+os.chdir(proj_name)
+p = subprocess.Popen(['virtualenv', 'venv'])
+p.communicate()
+
+execfile('venv/bin/activate_this.py', dict(__file__='venv/bin/activate_this.py'))
+
+pip.main(['install', 'django-extensions'])
+pip.main(['install', 'djangorestframework'])
+pip.main(['install', 'Django'])
+
+django_admin = os.getcwd() + '/venv/bin/django-admin.py'
+venv_python = os.getcwd() + '/venv/bin/python'
+
+p = subprocess.Popen([venv_python, django_admin, 'startproject', proj_name])
+p.communicate()
+
+os.chdir(proj_name)
+
+if app_name:
+    subprocess.Popen([venv_python, django_admin, 'startapp', app_name])
+
+if app_name:
+    sys.stdout.write("Django %s project successfully created with %s application name.\n" % (proj_name, app_name))
+else:
+    sys.stdout.write("Django %s project successfully created.\n" % proj_name)
+
